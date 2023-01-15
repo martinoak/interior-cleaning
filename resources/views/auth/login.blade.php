97ef0561ef29897b157ed7cfc30dd556360e8581
MarvelousMartin <oakk.martin@gmail.com>
2023-01-15 13:47:12 +0100

    change background in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,9 +18,9 @@
     <script src="https://unpkg.com/flowbite@1.6.0/dist/flowbite.min.js"></script>
 
 </head>
-<body>
+<body class="bg-gray-50 dark:bg-gray-900">
 
-<section class="bg-gray-50 dark:bg-gray-900">
+<section>
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             <img class="w-8 h-8 mr-2" src="/images/logo/logo.png" alt="logo">
