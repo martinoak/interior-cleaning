0b095f5c9f33d9f69b6a1978597ad65a3130d30f
Martin Dub <49438284+MarvelousMartin@users.noreply.github.com>
2023-01-16 10:25:04 +0100

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,4 +30,11 @@ class AuthController extends Controller
             return back()->with('error', 'Nesprávné přihlašovací údaje');
         }
     }
+    
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('dashboard');
+    }
 }
