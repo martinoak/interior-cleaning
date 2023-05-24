2782a4729df5506a45399745d321ef033660800c
Martin Dub <martin.dub@dek-cz.com>
2023-05-24 17:39:29 +0200

    Dump Laravel + PHP version

--- resources/views/partials/dev.blade.php
+++ resources/views/partials/dev.blade.php
@@ -1,8 +1,8 @@
-@if(str_contains(url()->current(), 'localhost'))
+@if(str_contains(url()->current(), 'mad.dek.cz'))
     <div class="row">
         <div class="col-lg-12">
             <div class="alert alert-danger text-center fw-bold">
-                Developer verze
+                Developer verze Laravel {{ Illuminate\Foundation\Application::VERSION }}, PHP {{ PHP_VERSION }}
             </div>
         </div>
     </div>
