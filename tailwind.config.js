9cb06129b8822529f2670c336ea5b8064734a656
Martin <oakk.martin@gmail.com>
2023-06-09 10:59:11 +0200

    Refactor admin panel 3, Tailwind dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ["./resources/views/**/*.latte"],
+    theme: {
+    extend: {},
+    },
+    plugins: [],
+}
+
